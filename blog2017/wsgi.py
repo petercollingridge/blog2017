da7087c0c1e7cba0d121ccac9201848aed713851
Peter Collingridge <peter.collingridge@gmail.com>
2016-11-13 23:20:13 +0000

    First commit

---
+++ blog2017/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for blog2017 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog2017.settings.dev")
+
+application = get_wsgi_application()
