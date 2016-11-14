f549f988370726fe960371b96077c355e28b24f4
Peter Collingridge <peter.collingridge@gmail.com>
2016-11-14 10:25:29 +0000

    Create models for adding sections to homepage

--- home/models.py
+++ home/models.py
@@ -2,14 +2,34 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+
+
+class BlogSection(models.Model):
+    title = models.CharField(max_length=255)
+    body = RichTextField(blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('body'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class HomePageSection(Orderable, BlogSection):
+    page = ParentalKey('HomePage', related_name='sections')
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    introduction = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('introduction', classname="full"),
+        InlinePanel('sections', label="Sections"),
     ]
