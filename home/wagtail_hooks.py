84ec0d84e4cb0080a9fba59e336da347ee9210d3
Peter Collingridge <peter.collingridge@gmail.com>
2018-03-26 13:19:55 +0100

    Add back comment panel to admin

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -1,8 +1,13 @@
-from wagtail.admin.rich_text.converters.html_to_contentstate import BlockElementHandler
-import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.core import hooks
+from django.utils.html import format_html, format_html_join
 
+import django_comments
+
+from wagtail.core import hooks
+from wagtail.admin.rich_text.converters.html_to_contentstate import BlockElementHandler
 from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 
 @hooks.register('register_rich_text_features')
@@ -31,3 +36,21 @@ def register_blockquote_feature(features):
         'from_database_format': {tag: BlockElementHandler(type_)},
         'to_database_format': {'block_map': {type_: tag}},
     })
+
+
+class CommentAdmin(ModelAdmin):
+    model = django_comments.models.Comment
+
+    menu_label = 'Comments'
+    menu_icon = 'list-ul'
+    menu_order = 200
+    add_to_settings_menu = False
+    list_display = ('user_name', 'submit_date', 'show_page_link', 'comment')
+
+    def show_page_link(self, obj):
+        return format_html("<a href='{url}'>{name}</a>", url=obj.content_object.url, name=obj.content_object)
+
+    show_page_link.short_description = "Page"
+
+
+modeladmin_register(CommentAdmin)
